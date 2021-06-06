10c53666864fc8b53cc7a749d9ccb039e6b2cdbb
Akhilesh00 <akhilesh00k1@gmail.com>
2021-06-06 23:43:55 +0530

    Add files via upload

---
+++ pattern1.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class pattern1 {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n= scan.nextInt();
+        scan.close();
+        
+        int nrows=n;
+        int nst=n;
+        int nsp=-1;
+        //rows
+
+        
+        for (int row = 1; row <=nrows; row++) {
+            //first stars
+            int digit=1;
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(digit+" ");
+                digit++;
+                
+            }
+            //second stars
+            for (int csp = 1; csp <=nsp; csp++) {
+                System.out.print("*"+" ");
+                
+            }
+            
+        
+            //changeline
+            System.out.println("");
+            //cdtn
+            nst--;
+            nsp+=2;
+
+           
+        }
+        
+    }
+
+
+
+    
+}
