c959bb69f17d4b2e2eb90c75a0518aeb987e2562
akhilesh <akhilesh00k1@gmail.com>
2021-06-23 22:59:35 +0530

    combine all elements of array to  form largest possible number

---
+++ Arrays/1d/largestno.java
@@ -0,0 +1,17 @@
+//package Arrays.1d;
+import java.util.Scanner;
+public class largestno {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        String s=scan.nextLine();
+        String [] stringarr=s.split(" ");
+        int [] arr=new int [stringarr.length];
+
+
+    }
+    public static int  greatest(int []arr){
+        
+    }
+    
+}
