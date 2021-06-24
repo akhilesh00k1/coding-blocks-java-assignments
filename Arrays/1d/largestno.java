322b03d24a43ca9529d992a7ce3c66bef8668deb
akhilesh <akhilesh00k1@gmail.com>
2021-06-24 20:38:29 +0530

    largest  no from array

--- Arrays/1d/largestno.java
+++ Arrays/1d/largestno.java
@@ -2,16 +2,30 @@
 import java.util.Scanner;
 public class largestno {
     public static void main(String[] args){
+        int n =4;
+        String str="123";
+        str =str.concat();
+         
         Scanner scan=new Scanner(System.in);
-        int n=scan.nextInt();
+        //int n=scan.nextInt();
         String s=scan.nextLine();
         String [] stringarr=s.split(" ");
-        int [] arr=new int [stringarr.length];
+        //check eacj sring length  and make all of them  sa me length by  adding xeroes
+
 
 
     }
-    public static int  greatest(int []arr){
-        
+    public static int greatest (String[] srr){
+        String ans="";
+        int count=0;
+        while (count<=srr.length-1){
+            for(int items=0;items<=srr.length-1;items++){
+
+            }
+
+            count++;
+        }
     }
+    
     
 }
