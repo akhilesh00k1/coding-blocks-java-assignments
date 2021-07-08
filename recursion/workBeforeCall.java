0417d3339b47cf1e7c17490a3323dd4f6ced51b1
akhilesh <akhilesh00k1@gmail.com>
2021-07-08 22:48:19 +0530

    work  beore and after call

--- recursion/workBeforeCall.java
+++ recursion/workBeforeCall.java
@@ -4,6 +4,7 @@ public class workBeforeCall{
 
     int n=5;
     pd(n);
+    pi(n);
 
     }
     public static void pd(int n){
@@ -13,6 +14,17 @@ public class workBeforeCall{
         }
         System.out.println(n);
         pd(n-1);
+
+    }
+    public static void pi(int n) {
+        //base case 
+        if(n==0){
+            return;
+        }
+        //no work beofre call
+        pi(n-1);
+        //orl when stack is falling
+        System.out.println(n);
 
     }
 }
